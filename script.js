6fc1525bfb073694eceb4ad5e920dc9c3daf6c12
basharinaa <temon-bash@yandex.ru>
2022-03-11 23:01:57 +0700

    bugs fixed

--- script.js
+++ script.js
@@ -1,42 +1,40 @@
-let popButton = document.querySelector('.person__edit-button');
-let closeButton = document.querySelector('.popup__close');
-let saveButton = document.querySelector('.popup__button')
-let popup = document.querySelector('.popup__container');
-let nameContainer = document.querySelector('.popup__name');
-let nameValue = document.querySelector('.person__name');
-let employmentContainer = document.querySelector('.popup__employment');
-let employmentValue = document.querySelector('.person__employment');
-nameContainer.setAttribute('value', nameValue.textContent);
-employmentContainer.setAttribute('value', employmentValue.textContent);
-let likeButton = document.querySelectorAll('.card__like-button');
+const popButton = document.querySelector('.person__edit-button');
+const closeButton = document.querySelector('.popup__close');
+const saveButton = document.querySelector('.popup__form')
+const popup = document.querySelector('.popup');
+const nameContainer = document.querySelector('.popup__input_type_name');
+const nameValue = document.querySelector('.person__name');
+const employmentContainer = document.querySelector('.popup__input_type_employment');
+const employmentValue = document.querySelector('.person__employment');
 
-function hidePopup() {
-    popup.classList.add('popup__container_hide');
-};
+const likeButton = document.querySelectorAll('.card__like-button');
 
 function showPopup() {
-    popup.classList.remove('popup__container_hide');
+    nameContainer.setAttribute('value', nameValue.textContent);
+    employmentContainer.setAttribute('value', employmentValue.textContent);
+    popup.classList.add('popup_opened');
+};
+
+function closePopup() {
+    popup.classList.remove('popup_opened');
 };
 
 function saveProfile(evt) {
 
-    nameValue.textContent = document.querySelector('.popup__name').value;
-    employmentValue.textContent = document.querySelector('.popup__employment').value;
-    popup.classList.add('popup__container_hide');
+    nameValue.textContent = document.querySelector('.popup__input_type_name').value;
+    employmentValue.textContent = document.querySelector('.popup__input_type_employment').value;
+    popup.classList.remove('popup_opened');
     evt.preventDefault();
 };
 
-// function activateButton(any) {
-//     any.classList.toggle('card__like-button_black');
-// };
 
 popButton.addEventListener('click', showPopup);
-closeButton.addEventListener('click', hidePopup);
-saveButton.addEventListener('click', saveProfile);
-// likeButton.addEventListener('click', activateButton);
+closeButton.addEventListener('click', closePopup);
+saveButton.addEventListener('submit', saveProfile);
 
-likeButton.forEach(element => {
-    element.addEventListener('click', () => {
-        element.classList.toggle('card__like-button_black')
-    })
-});
+
+// likeButton.forEach(element => {
+//     element.addEventListener('click', () => {
+//         element.classList.toggle('card__like-button_black')
+//     })
+// });
